c2297670794ce0dedfde66addb425e8f109653b2
Rafael <bboyrafinhazika@ufu.br>
2022-05-26 22:23:45 -0300

    Atualização do projeto

--- scr/main.c
+++ scr/main.c
@@ -1,7 +1,78 @@
 #include <stdlib.h>
+#include <stdint.h>
 
-int main(void)
+#define STM32_RCC_BASE              0x40023800
+#define STM32_GPIOC_BASE            0x48000800U
+#define STM32_RCC_AHB1ENR_OFFSET    0x0030
+#define STM32_GPIO_MODER_OFFSET     0x0000
+#define STM32_GPIO_OTYPER_OFFSET    0x0004
+#define STM32_GPIO_PUPDR_OFFSET     0x000c
+#define STM32_GPIO_BSRR_OFFSET      0x0018
+#define STM32_RCC_AHB1ENR           (STM32_RCC_BASE+STM32_RCC_AHB1ENR_OFFSET)
+#define STM32_GPIOC_MODER           (STM32_GPIOC_BASE+STM32_GPIO_MODER_OFFSET)
+#define STM32_GPIOC_OTYPER          (STM32_GPIOC_BASE+STM32_GPIO_OTYPER_OFFSET)
+#define STM32_GPIOC_PUPDR           (STM32_GPIOC_BASE+STM32_GPIO_PUPDR_OFFSET)
+#define STM32_GPIOC_BSRR            (STM32_GPIOC_BASE + STM32_GPIO_BSRR_OFFSET)
+#define RCC_AHB1ENR_GPIOCEN         (1 << 2)
+#define GPIO_MODER_INPUT            (0)
+#define GPIO_MODER_OUTPUT           (1)
+#define GPIO_MODER_ALT              (2)
+#define GPIO_MODER_ANALOG           (3)
+#define GPIO_MODER13_SHIFT          (26)
+#define GPIO_MODER13_MASK           (3 << GPIO_MODER13_SHIFT)
+#define GPIO_OTYPER_PP              (0) 
+#define GPIO_OTYPER_OD              (1) 
+#define GPIO_OT13_SHIFT             (13)
+#define GPIO_OT13_MASK              (1 << GPIO_OT13_SHIFT)
+#define GPIO_PUPDR_NONE             (0) 
+#define GPIO_PUPDR_PULLUP           (1) 
+#define GPIO_PUPDR_PULLDOWN         (2) 
+#define GPIO_PUPDR13_SHIFT          (26)
+#define GPIO_PUPDR13_MASK           (3 << GPIO_PUPDR13_SHIFT)
+#define GPIO_BSRR_SET(n)            (1 << (n))
+#define GPIO_BSRR_RST(n)            (1 << (n + 16))
+
+static const char fw_version[] = {'V', '1', '.', '0'};
+static uint32_t led_status;
+
+int main(int argc, char *argv[])
 {
+    uint32_t reg;
+    uint32_t *pRCC_AHB1ENR = (uint32_t *)STM32_RCC_AHB1ENR;
+    uint32_t *pGPIOC_MODER = (uint32_t *)STM32_GPIOC_MODER;
+    uint32_t *pGPIOC_OTYPER = (uint32_t *)STM32_GPIOC_OTYPER;
+    uint32_t *pGPIOC_PUPDR = (uint32_t *)STM32_GPIOC_PUPDR;
+    uint32_t *pGPIOC_BSRR = (uint32_t *)STM32_GPIOC_BSRR;
+    uint32_t LED_DELAY = 10000;
+
+    reg = *pRCC_AHB1ENR;
+    reg |= RCC_AHB1ENR_GPIOCEN;
+    *pRCC_AHB1ENR = reg;
+    
+    reg = *pGPIOC_MODER;
+    reg &= ~(GPIO_MODER13_MASK);
+    reg |= (GPIO_MODER_OUTPUT << GPIO_MODER13_SHIFT);
+    *pGPIOC_MODER = reg;
+
+    reg = *pGPIOC_OTYPER;
+    reg &= ~(GPIO_OT13_MASK);
+    reg |= (GPIO_OTYPER_PP << GPIO_OT13_SHIFT);
+    *pGPIOC_OTYPER = reg;
+
+    reg = *pGPIOC_PUPDR;
+    reg &= ~(GPIO_PUPDR13_MASK);
+    reg |= (GPIO_PUPDR_NONE << GPIO_PUPDR13_SHIFT);
+    *pGPIOC_PUPDR = reg;
+
     while(1)
-    return EXIT_FAILURE;
+    {
+        *pGPIOC_BSRR = GPIO_BSRR_SET(13);
+        led_status = 0;
+        for(uint32_t i = 0; i < LED_DELAY; i++);
+        *pGPIOC_BSRR = GPIO_BSRR_RST(13);
+        led_status = 1;
+        for(uint32_t i = 0; i < LED_DELAY; i++);
+    }
+
+    return EXIT_SUCCESS;
 }
--- scr/startup.c
+++ scr/startup.c
@@ -1,42 +1,79 @@
 #include <stdint.h>
 
 #define SRAM_START 0x20000000U
-#define SRAM_SIZE (128U * 1024)
+#define SRAM_SIZE (128U * 1024U)
 #define SRAM_END  SRAM_START + SRAM_SIZE
 
 #define STACK_START SRAM_END
 
 void main();
 void reset_handler();
-void nmi_handler();
+void nmi_handler        (void) __attribute__ ((weak, alias("default_handler")));
+void hardfault_handler  (void) __attribute__ ((weak, alias("default_handler")));
+void memmanage_handler  (void) __attribute__ ((weak, alias("default_handler")));
+void busfault_handler   (void) __attribute__ ((weak, alias("default_handler")));
+void usagefault_handler (void) __attribute__ ((weak, alias("default_handler")));
+void svc_handler        (void) __attribute__ ((weak, alias("default_handler")));
+void debugmon_handler   (void) __attribute__ ((weak, alias("default_handler")));
+void pendsv_handler     (void) __attribute__ ((weak, alias("default_handler")));
+void systick_handler    (void) __attribute__ ((weak, alias("default_handler")));
+
+extern uint32_t _sdata;    /* Inicio da secao .data */
+extern uint32_t _edata;    /* Fim da secao .data */
+extern uint32_t _la_data;  /* Endereco de carga na RAM da secao .data */
+extern uint32_t _etext;
+extern uint32_t _sbss;     /* Inicio da secao .bss */
+extern uint32_t _ebss;     /* Fim da secao .bss */
 
 uint32_t vectors[] __attribute__((section (".isr_section"))) =
 {
     STACK_START,
     (uint32_t)reset_handler,
     (uint32_t)nmi_handler,
+    (uint32_t)reset_handler, 
+    (uint32_t)nmi_handler, 
+    (uint32_t)hardfault_handler, 
+    (uint32_t)memmanage_handler, 
+    (uint32_t)busfault_handler,
+    (uint32_t)usagefault_handler,
     0,
     0,
     0,
     0,
+    (uint32_t)svc_handler,
+    (uint32_t)debugmon_handler,
     0,
-    0,
-    0,
-    0,
-    0,
-    0,
-    0,
-    0,
-    0
+    (uint32_t)pendsv_handler,
+    (uint32_t)systick_handler,
 
 };
 
 void reset_handler(void)
 {
+
+    /* Copia a secao .data para a RAM */
+    uint32_t i;
+    uint32_t size = (uint32_t)&_edata - (uint32_t)&_sdata;
+    uint8_t *pDst = (uint8_t*)&_sdata; 
+    uint8_t *pSrc = (uint8_t*)&_etext;
+
+    for(i = 0; i < size; i++)
+    {
+        *pDst++ = *pSrc++;
+    }
+
+    /* Preenche a secao .bss com zero */
+    size = (uint32_t)&_ebss - (uint32_t)&_sbss;
+    pDst = (uint8_t*)&_sbss;
+    for(i = 0 ; i < size; i++)
+    {
+        *pDst++ = 0;
+    }
+
     main();
 }
 
-void nmi_handler(void)
+void default_handler(void)
 {
     while(1);
 }
